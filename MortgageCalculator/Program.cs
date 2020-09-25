69e17e84608f7c30237892c1d0e1542a440889ca
Stela <sjorgo@ritech.co>
2020-09-25 15:26:00 +0200

    added_amortization_schedule

--- MortgageCalculator/Program.cs
+++ MortgageCalculator/Program.cs
@@ -8,7 +8,6 @@ namespace MortgageCalculator
         {
             Console.WriteLine("Enter loan's principal: ");
             var input_Principal = Console.ReadLine();
-
             float principal;
 
             while (string.IsNullOrEmpty(input_Principal) || !float.TryParse(input_Principal, out principal))
@@ -18,9 +17,8 @@ namespace MortgageCalculator
             }
 
   
-            Console.WriteLine("Enter monthly interest rate: ");
+            Console.WriteLine("Enter yearly interest rate: ");
             var input_rate = Console.ReadLine();
-
             float rate;
 
             while (string.IsNullOrEmpty(input_rate) || !float.TryParse(input_rate, out rate))
@@ -32,28 +30,44 @@ namespace MortgageCalculator
 
             Console.WriteLine("Enter number of years: ");
             var input_years = Console.ReadLine();
-
             int years;
+
             while (string.IsNullOrEmpty(input_years) || !int.TryParse(input_years, out years))
             {
                 Console.WriteLine("Wrong input! Please try again: ");
                 input_years = Console.ReadLine();
             }
 
-
+            var monthly_rate = rate / 12;
             var months = years * 12;
-            var mounthlyMortgage = 0.0;
+            var monthlyMortgage = 0.0;
 
-            if (rate != 0)
+            monthlyMortgage = (monthly_rate * principal * Math.Pow(1 + monthly_rate, months))/(Math.Pow(1 + monthly_rate, months) - 1);
+            Console.WriteLine(string.Format("Monthly mortgage is: " + monthlyMortgage.ToString("C")));
+
+            amortizationTable(principal, monthlyMortgage, rate, months);
+
+        }
+
+        public static void amortizationTable(float principal, double monthlyMortgage, float rate, float months)
+        {
+            Console.WriteLine("\t  Amortization Schedule");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("|Month| \t |Interest| \t |Principal|");
+
+            DateTime date = DateTime.Today;
+            double amortizationP = principal;
+
+
+            for (var i = 0; i < months; i++)
             {
-                mounthlyMortgage = (rate * principal * Math.Pow(1 + rate, months))/(Math.Pow(1 + rate, months) - 1);
-            }
-            else
-            {
-                mounthlyMortgage = principal / months;
+                var interest = amortizationP * rate / 12;
+                var principal_payment = monthlyMortgage - interest;
+                amortizationP = amortizationP - principal_payment;
+                Console.WriteLine("{0} \t {1} \t {2}", date.ToString("MMMM-yyyy"), interest.ToString("C"), principal_payment.ToString("C"));
+                date = date.AddMonths(1);
             }
 
-            Console.WriteLine(string.Format("Monthly mortgage is: " + mounthlyMortgage.ToString("C")));
         }
     }
 }
